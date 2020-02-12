399ee84af70c10b08e3addcf933058a5682e17b3
seshasainagadevara <imagtorel@gmail.com>
2020-02-12 11:12:15 +0530

    modify

---
+++ src/app.js
@@ -0,0 +1,4 @@
+"use-strict";
+import request from "request";
+
+request ? .post();
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require("path");
 module.exports = {
     entry: "./src/app.js",
+    target: "node",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "app.output.js"
+    },
+    module: {
+        rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
     }
 };
